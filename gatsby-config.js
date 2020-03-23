59e0726d50a84ae54d7a780eb70d3d894944021f
Rahmat Aligos <rahmat@clloh.com>
2020-03-23 10:26:34 +0700

    feature: add site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,21 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Ge`,
+    author: `Ge Rahmat`,
+    description: `Personal blog by `,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/rahmataligos`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/aligos`,
+      },
+      {
+        name: `stackoverflow`,
+        url: `https://stackoverflow.com/users/6366525/rahmat-aligos`,
       },
     ],
   },
